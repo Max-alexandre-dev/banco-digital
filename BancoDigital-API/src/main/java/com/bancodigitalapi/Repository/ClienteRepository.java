2cd5fdf709a3a99b77c676209c9dd24536232ff6
RyanIsraelDev <ryan.levado6@gmail.com>
2026-02-12 22:44:30 -0300

    Deletando api (problemas técnicos)

--- BancoDigital-API/src/main/java/com/bancodigitalapi/Repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bancodigital-api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bancodigital-api.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, int>{
-}
